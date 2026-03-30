2fd8d5b7aa8071bf1464b481806da58a10e95bb6
mohamedelnazerr <mohamedelnazer1002@gmail.com>
2026-03-30 13:55:55 +0200

    Add hello_stage1.sh script for stage one greeting

---
+++ scripts/hello_stage1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "==============================="
+echo "  Hello from Stage One script! "
+echo "  Build: $BUILD_NUMBER"
+echo "  Job:   $JOB_NAME"
+echo "==============================="

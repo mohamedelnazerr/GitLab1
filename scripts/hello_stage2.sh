7d3bc4ec96a2e9a615c88bd00ae3c6c980ae68c7
mohamedelnazerr <mohamedelnazer1002@gmail.com>
2026-03-30 13:58:58 +0200

    Create hello_stage2.sh

---
+++ scripts/hello_stage2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "==============================="
+echo "  Hello from Stage Two script! "
+echo "  Workspace: $WORKSPACE"
+echo "  Branch:    $GIT_BRANCH"
+echo "==============================="
